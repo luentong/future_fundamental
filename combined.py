ac42c78cd6acddeb645cd14a0b944dafe52ff39c
unknown <qinqining>
2022-02-18 12:54:22 +0800

    20220118

--- combined.py
+++ combined.py
@@ -192,7 +192,7 @@ for key in idea:
     # 菜粕菜油自己改
     if key == "菜粕&菜油":
         topop.append("菜粕&菜油")
-        toadd.append(["菜粕", "0"])
+        toadd.append(["菜粕", "-0.3"])
         toadd.append(["菜油", "0"])
     # # 玻璃纯碱自己改
     # if key == "纯碱":
