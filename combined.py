ad3d161f53a1c7189a6d13f29b1e26e188cc2dbe
unknown <qinqining>
2022-04-13 21:07:14 +0800

    20220413

--- combined.py
+++ combined.py
@@ -214,8 +214,8 @@ for key in idea:
     # 菜粕菜油自己改
     if key == "菜粕&菜油":
         topop.append("菜粕&菜油")
-        toadd.append(["菜粕", "0"])
-        toadd.append(["菜油", "0"])
+        toadd.append(["菜粕", "1"])
+        toadd.append(["菜油", "1"])
 
 for i in topop:
     if i in idea:
